10e82f986182ce2e807290702bae37c0ab08bbf1
NICK MUGO NJURU <nicknjuru13@gmail.com>
2023-07-22 09:40:21 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,19 @@
+const divide=(number1,number2,result)=>{
+    number1="2000";
+    number2="100";
+    parseInt=("number1",10);
+    parseInt=("number2",10);
+    return number1/number2;}
+console.log(divide());
+const square=(n1)=>{
+    n1="2";
+    parseInt=("n1",10);
+    return n1*n1; 
+}
+const add=(num1,num2)=>{
+    num1=4;
+    num2=3;
+    parseInt=("num1",10);
+    parseInt=("num2",10);
+    return num1+num2;
+}
